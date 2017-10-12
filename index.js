9d9db7b57cfa8fc9b13db5ed60925ad0e234382a
XibXor <andrew@bamsoftwaregroup.com>
2017-10-12 15:14:59 -0700

    all

---
+++ index.js
@@ -0,0 +1,16 @@
+const contractAddr = "0x18e1438312262214db7ad40def5542f9e783560a";
+const cvs = document.getElementById("cvs");
+const ctx = cvs.getContext("2d");
+
+window.onload = function(){
+	for (var x = 0; x < 2; ++x){
+		for (var y = 0; y < 2; ++y){
+			((x,y) => getStorageAt(contractAddr, 128 * x + y + 2, (err, color) => {
+				for (var i = 1; i <= 8; ++i){
+					ctx.fillStyle = color.slice(2, 2 + 8*i);
+					ctx.fillRect(x, y + 8 - i, 1, 1);
+				}
+			}))(x,y);
+		}
+	}
+};
