3a1025da06320394e25d8088fa978fc225c21abd
XibXor <andrew@bamsoftwaregroup.com>
2017-10-12 15:25:40 -0700

    all

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cvs = document.getElementById("cvs");
 const ctx = cvs.getContext("2d");
 
 setTimeout(function(){
-	for (var x = 250; x < 260; ++x){
-		for (var y = 127; y < 128; ++y){
+	for (var x = 0; x < 1024; ++x){
+		for (var y = 0; y < 128; ++y){
 			((x,y) => web3.eth.getStorageAt(contractAddr, 128 * x + y + 2, (err, color) => {
 				for (var i = 1; i <= 8; ++i){
 					ctx.fillStyle = "#" + color.slice(2, 2 + 8*i);
-					ctx.fillRect(x, y + 8 - i, 1, 1);
+					ctx.fillRect(x, 8*y + 8 - i, 1, 1);
 				}
 			}))(x,y);
 		}
