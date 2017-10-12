ed56ffcba22d85d0a865a33b2cab38195509fb02
XibXor <andrew@bamsoftwaregroup.com>
2017-10-12 15:16:05 -0700

    all

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ctx = cvs.getContext("2d");
 window.onload = function(){
 	for (var x = 0; x < 2; ++x){
 		for (var y = 0; y < 2; ++y){
-			((x,y) => getStorageAt(contractAddr, 128 * x + y + 2, (err, color) => {
+			((x,y) => web3.eth.getStorageAt(contractAddr, 128 * x + y + 2, (err, color) => {
 				for (var i = 1; i <= 8; ++i){
 					ctx.fillStyle = color.slice(2, 2 + 8*i);
 					ctx.fillRect(x, y + 8 - i, 1, 1);
