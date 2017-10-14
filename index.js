d7a813254af4ac46b7c21bfe64179a5abbbf881f
XibXor <andrew@bamsoftwaregroup.com>
2017-10-14 13:14:24 -0700

    all

--- index.js
+++ index.js
@@ -2,16 +2,49 @@ const contractAddr = "0x18e1438312262214db7ad40def5542f9e783560a";
 const cvs = document.getElementById("cvs");
 const ctx = cvs.getContext("2d");
 
-setTimeout(function(){
-	for (var x = 0; x < 1024; ++x){
-		for (var y = 0; y < 128; ++y){
-			((x,y) => web3.eth.getStorageAt(contractAddr, 128 * x + y + 2, (err, color) => {
-				for (var i = 1; i <= 8; ++i){
-					ctx.fillStyle = "#000000";
-					//ctx.fillStyle = "#" + color.slice(2, 2 + 8*i);
-					ctx.fillRect(x, 8*y + 8 - i, 1, 1);
-				}
-			}))(x,y);
-		}
+const numThreads = 4;
+
+var i = -1;
+setTimeout(() => {
+	while (numThreads--){
+		var thread = {i: i};
+		thread.cb = cb.bind(thread);
+		thread.cb();
 	}
 }, 1000);
+
+
+const unpair = (z) => {
+	const w = Math.ceil(
+		(Math.sqrt(8 * z + 1) - 1) / 2
+	);
+	const t = w * (w + 1) / 2;
+	const y = z - t;
+	const x = w - y;
+
+	return [x,y];
+};
+
+const pair = (x,y) => (
+	(x + y + 1) * (x + y) / 2 + y;
+);
+
+function cb(err, val){
+	if (val){
+		val
+			.slice(2)
+			.split("")
+			.map(v => ("000" + parseInt(v,16).toString(2)).slice(-4))
+			.join("");
+			.forEach((v, idx) => {
+				const coords = unpair(this.i + idx);
+				ctx.fillStyle = v == "0" ? "#000" : "#fff";
+				ctx.fillRect(coords[0], coords[1], 1, 1);
+			});
+	}
+	if ((++i) == 4096){
+		return;
+	}
+	thread.i = i;
+	web3.eth.getStorageAt(contractAddr, i, this.cb);
+}
